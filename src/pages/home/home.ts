6e9dea3d6616880c2bdd95b499248f48e4f765cd
leon_xi <leon_xi@163.com>
2019-08-03 23:34:04 +0800

    更新首页显示内容

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,15 @@ import { NavController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   template: `<ion-content>
-  
+  <ion-grid>
+    <ion-row>
+      <ion-card>
+        <img src="imgs/github.jpg" />
+        <div class="card-title">GitHub</div>
+        <div class="card-subtitle">41 messages / 2 repositories</div>
+      </ion-card>
+    </ion-row>
+  </ion-grid>
   </ion-content>`
 })
 export class HomePage {
