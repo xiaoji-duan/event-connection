22657189ef0b2e7abc6abea014fb55d2cbbde08d
leon_xi <leon_xi@163.com>
2019-08-04 15:50:35 +0800

    增加画面迁移

--- src/pages/pushes/pushes.ts
+++ src/pages/pushes/pushes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Constants } from '../../utils/constants';
 
 @Component({
   selector: 'page-pushes',
@@ -8,7 +9,7 @@ import { NavController } from 'ionic-angular';
       <div class="card-title">GitHub</div>
       <div class="card-subtitle">41 messages / 2 repositories</div>
     </ion-card>
-    <ion-card class="button-card">
+    <ion-card class="button-card" (click)="addPush()">
       <div class="card-icon">
         <ion-icon ios="md-add" md="md-add"></ion-icon>
       </div>
@@ -17,8 +18,15 @@ import { NavController } from 'ionic-angular';
 })
 export class PushesPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public modalCtrl: ModalController) {
 
   }
 
+  addPush() {
+    let modal = this.modalCtrl.create(Constants.PAGE._COLLECTION_PAGE);
+    modal.onDidDismiss((data)=>{
+    });
+    modal.present();
+  }
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+import { PushesPage } from "../../pages/pushes/pushes";
+import { CollectionPage } from "../../pages/collection/collection";
+
+export class Constants {
+  static PAGE = {
+    _PUSHES_PAGE: PushesPage,
+    _COLLECTION_PAGE: CollectionPage
+  };
+}
