59788e3468e8a4a0bb37235f45bc01fbec551bb4
leon_xi <leon_xi@163.com>
2019-08-04 10:41:15 +0800

    更新样式

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,11 @@ import { NavController } from 'ionic-angular';
       <div class="card-title">GitHub</div>
       <div class="card-subtitle">41 messages / 2 repositories</div>
     </ion-card>
+    <ion-card class="button-card">
+      <div class="card-icon">
+        <ion-icon ios="ios-add-circle-outline" md="ios-add-circle-outline"></ion-icon>
+      </div>
+    </ion-card>
   </ion-content>`
 })
 export class HomePage {
