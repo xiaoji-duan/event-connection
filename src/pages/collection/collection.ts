fb3f89694a38bac691301ed8cf68e9197a7b7af8
leon_xi <leon_xi@163.com>
2019-08-04 15:55:53 +0800

    更新资源文件

--- src/pages/collection/collection.ts
+++ src/pages/collection/collection.ts
@@ -15,13 +15,13 @@ import { NavController } from 'ionic-angular';
               <img src="assets/imgs/fir.im/propeller.svg" class="fir-propeller">
             </div>
             <div class="firim-help">
-            <button ion-button color="dark" class="border" clear round small (click)="help()">设置帮助</button>
+            <button ion-button color="dark" class="border" clear round small (click)="help()">添加</button>
             </div>
           </ion-row>
           <ion-row align-items-center justify-content-center class="full-width github-color">
             <ion-icon ios="logo-github" md="logo-github" class="github-logo"></ion-icon>
             <div class="github-help">
-            <button ion-button color="dark" class="border" clear round small (click)="help()">设置帮助</button>
+            <button ion-button color="dark" class="border" clear round small (click)="help()">添加</button>
             </div>
           </ion-row>
         </ion-grid>
