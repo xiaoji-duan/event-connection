f185621790989ced1015179f69da3ecae4d3bb37
leon_xi <leon_xi@163.com>
2019-08-04 15:59:39 +0800

    更新

--- src/pages/collection/collection.ts
+++ src/pages/collection/collection.ts
@@ -18,6 +18,7 @@ import { NavController } from 'ionic-angular';
             <button ion-button color="dark" class="border" clear round small (click)="help()">添加</button>
             </div>
           </ion-row>
+          <ion-row><p></p></ion-row>
           <ion-row align-items-center justify-content-center class="full-width github-color">
             <ion-icon ios="logo-github" md="logo-github" class="github-logo"></ion-icon>
             <div class="github-help">
