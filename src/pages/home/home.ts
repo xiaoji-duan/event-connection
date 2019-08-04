1403b77a5927d66545543dc3fbd69fefdbdec147
leon_xi <leon_xi@163.com>
2019-08-04 10:57:05 +0800

    更新样式

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,7 @@ import { NavController } from 'ionic-angular';
     </ion-card>
     <ion-card class="button-card">
       <div class="card-icon">
-        <ion-icon ios="ios-add-circle" md="ios-add-circle"></ion-icon>
+        <ion-icon ios="md-add" md="md-add"></ion-icon>
       </div>
     </ion-card>
   </ion-content>`
